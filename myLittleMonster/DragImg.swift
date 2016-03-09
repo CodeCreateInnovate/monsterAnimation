65dc86ce4c2a6a6785bdd530128453e635d04739
Erick Truong <Truong_Home@Ericks-MBP.home>
2016-03-08 21:14:03 -0500

    Initial Commit of Monster Animation

---
+++ myLittleMonster/DragImg.swift
@@ -0,0 +1,47 @@
+//
+//  DragImg.swift
+//  myLittleMonster
+//
+//  Created by ERICK TRUONG on 12/30/15.
+//  Copyright © 2015 Code Create Innovate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DragImg: UIImageView {
+    
+    var originialPosition: CGPoint!
+    var dropTarget: UIView?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        originialPosition = self.center
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if let touch = touches.first, let target = dropTarget {
+            let position = touch.locationInView(self.superview)
+            
+            if CGRectContainsPoint(target.frame, position) {
+                NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "onTargetDropped", object: nil))
+            }
+        }
+        self.center = originialPosition
+    }
+}
